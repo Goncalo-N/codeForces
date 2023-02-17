6ab5dcd2f3720d6842b6542ce3c1c78bbcd91a3f
1201525GoncaloNogueira <nogueira.goncalo15@gmail.com>
2023-02-17 03:59:29 +0000

    A. Way Too Long Words done
    Basic String reading exercise done just to test basic java String knowledge

---
+++ src/main/java/AWayTooLongWords.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AWayTooLongWords { // Problem link : https://codeforces.com/problemset/problem/71/A
+    public static void AWayTooLongWords(){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int counter;
+        for(int i = 0; i<n;i++){
+        counter = 0;
+        String str = sc.next();
+        if(str.length()>=10) {
+            for (int j = 0; j < str.length(); j++) {
+                if (j == 0 || j == str.length() - 1) {
+                    if (counter != 0)
+                        System.out.print(counter);
+                    System.out.print(str.charAt(j));
+                } else {
+                    counter++;
+                }
+
+            }
+        }else{
+            System.out.print(str);
+        }
+        if(i!=n-1)
+            System.out.println();
+
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int counter;
-        for(int i = 0; i<n;i++){
-            counter = 0;
-            String str = sc.next();
-            if(str.length()>=10) {
-                for (int j = 0; j < str.length(); j++) {
-                    if (j == 0 || j == str.length() - 1) {
-                        if (counter != 0)
-                            System.out.print(counter);
-                        System.out.print(str.charAt(j));
-                    } else {
-                        counter++;
-                    }
+    public static void main(String args[]) {
+    //AWayTooLongWords.AWayTooLongWords();
 
-                }
-            }else{
-                System.out.print(str);
-            }
-        System.out.println();
-
-        }
     }
 }
