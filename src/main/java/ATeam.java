dbff333160c5be094bd1c3f9d256509d63604405
1201525GoncaloNogueira <nogueira.goncalo15@gmail.com>
2023-02-17 15:01:47 +0000

    ATeam
    ATeam problem submitted,  and accepted.

---
+++ src/main/java/ATeam.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ATeam {
+    public static void ATeam(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int total=0;
+        for(int i = 0; i<n; i++){
+
+            a= sc.nextInt();
+            b= sc.nextInt();
+            c= sc.nextInt();
+            if(a+b+c >=2)
+                total++;
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+        System.out.println(total);
+    }
+}
